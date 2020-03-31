97d9a3a881126e1489f966e2e5f9b95af3d1af05
Dr. X <mountrouidoux@cofc.edu>
2020-03-31 14:35:33 -0400

    exploits delete comments, ports add file for devices

--- IoT_openPorts.py
+++ IoT_openPorts.py
@@ -12,52 +12,32 @@ SHODAN_API_KEY = str(myKey)
 api = shodan.Shodan(SHODAN_API_KEY)
 f.close()
 
-# Wrap the request in a try/ except block to catch errors
-#devices = ['webcam',  'TP-Link', 'axis', 'D-Link', 'Dericam', 'Foscam', 'Icam', 'KaiKong', 'Loftek', 'Coolcam', 'Netcam', 'Panasonic', 'Polaroid', 'Pyle', 'Safehome', 'Sricam', 'Vstarcam', 'Wanscam', 'router', 'netgear', 'linksys', 'Asus', 'Tenda', 'Amazon Echo', 'Google Home', 'printer', 'Brother', 'HP OfficeJet', 'HP LaserJet', 'Canon', 'Epson']
-devices = ['webcam',  'TP-Link', 'axis', 'D-Link', 'Dericam', 'Panasonic', 'router', 'netgear', 'linksys', 'Asus', 'Tenda', 'Amazon Echo', 'printer', 'Brother', 'HP OfficeJet', 'HP LaserJet', 'Canon', 'Epson']
-# lights = ['', 'router', 'Amazon Echo', 'printer', 'light']
-exploits_type = ['dos', 'local', 'remote', 'webapps']
-exploits_platform = ['windows', 'php', 'linux', 'hardware']
+# read devices from file
+input_file_devices = 'devices.csv'
+devices_data = dict()
+with open(input_file_devices, 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        devices_data[row[0]] = (row[1:])
 
 print('Device, number of telnet, number of http, number of https, port 8081, port 8080, port 21, port 137, number of samba, number of netbios')
-for device in devices:
-    try:
-        results = api.search(device)
-        num_telnet = api.search(device + ' port:23')
-        num_http = api.search(device + ' port:80')
-        num_https = api.search(device + ' port:443')
-        num_8081 = api.search(device + ' port:8081')
-        num_8080 = api.search(device + ' port:8080')
-        num_ftp = api.search(device + ' port:21')
-        num_137 = api.search(device + ' port:137')
-        num_samba = api.search(device+ ' product:samba')
-        #num_netbios = api.search('webcam product:netbios')
-        print(device + ', ' + str(results['total']) + ', ' + str(num_telnet['total']) + ', ' + str(num_http['total']) + ', ' + str(num_https['total']) + ', ' + str(num_8081['total']) + ', ' + str(num_8080['total']) + ', ' + str(num_ftp['total']) + ', ' + str(num_137['total']) + ', ' + str(num_samba['total']))
-        time.sleep(20)
-    except Exception as e:
-        print(e)
-
-# change word and list
-#for device in webcams:
-#    print('Searching for '+ device)
-#    print('=================================================================================')
-    # Wrap the request in a try/ except block to catch errors
-#    try:
-        # Search Shodan
-        # change word and list
-#        print('Webcam, number of telnet, number of http, number of https, port 8081, port 137, number of samba, number of netbios')
-#        results = api.search(device + ' webcam')
-#        telnet = api.search(device + ' webcam port:23')
-        #http = api.search(device + ' webcam port:80')
-        #http1 = api.search(device + ' webcam port:8080')
-        #http2 = api.search(device + ' webcam port:8081')
-        #samba = api.search(device + ' webcam product:samba')
-        #netbios = api.search(device + ' webcam port:137')
-
-        # Show the results
-#        print('Results found: {}'.format(telnet['total']))
-        #print('Port {}'.format(results['port']))
-        #print(telnet['total'] + ', ' + http['total'] + ', ' + http1['total'] + ', ' + http2['total'] + ', ' + samba['total'] + ', ' + netbios['total'] )
-#        print('')
-#    except Exception as e:
-#        print(e)
+for dev_item in devices_data:
+    #          # Search Shodan
+    f.write('Searching for '+ dev_item)
+    for model in devices_data[dev_item]:
+        try:
+                results = api.search(model)
+                num_telnet = api.search(device + ' port:23')
+                #     num_http = api.search(device + ' port:80')
+                #     num_https = api.search(device + ' port:443')
+                #     num_8081 = api.search(device + ' port:8081')
+                #     num_8080 = api.search(device + ' port:8080')
+                #     num_ftp = api.search(device + ' port:21')
+                #     num_137 = api.search(device + ' port:137')
+                #     num_samba = api.search(device+ ' product:samba')
+                # #num_netbios = api.search('webcam product:netbios')
+                # print(device + ', ' + str(results['total']) + ', ' + str(num_telnet['total']) + ', ' + str(num_http['total']) + ', ' + str(num_https['total']) + ', ' + str(num_8081['total']) + ', ' + str(num_8080['total']) + ', ' + str(num_ftp['total']) + ', ' + str(num_137['total']) + ', ' + str(num_samba['total']))
+                print(device + ', ' + str(results['total']) + ', ' + str(num_telnet['total']))
+                time.sleep(20)
+        except Exception as e:
+                print(e)
